109a2d7982b8a05d85ee77c6ac8ef20dd355cf0f
=guedj_m <maxime.guedj@epitech.eu>
2016-03-29 23:39:47 -0700

    Initial commit

---
+++ test/Test.js
@@ -0,0 +1,13 @@
+"use strict";
+const fs = require("fs");
+const validator = require("is-my-json-valid");
+
+
+describe("Testing oauth2-utils", function () {
+  const validatorFile = JSON.parse(fs.readFileSync("./ressources/schemas/full.json"));
+  const toValid = JSON.parse(fs.readFileSync("./ressources/models/tmp.json"))
+  
+  const validate = validator(validatorFile, { verbose: true, greedy: true});
+  console.log(validate(toValid));
+  console.log(validate.errors);
+});
